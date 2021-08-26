d4240fd5bf4edb2bf290acd6cc79ce853212c46a
developer-suhag <developersuhag@gmail.com>
2021-08-27 02:28:37 +0600

    show country details dynamicly

--- js/country.js
+++ js/country.js
@@ -14,7 +14,33 @@ const displayCountries = countries => {
         <img src="${country.flag}"/>
             <h3>${country.name}</h3>
             <p>Capital: ${country.capital}</p>
+            <button onclick="loadCountryByName('${country.name}')">Details</button>
         `;
         allCountries.appendChild(div)
     });
+};
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showDetails(data[0]))
+    document.getElementById('title').style.display = 'block';
+};
+
+const showDetails = country => {
+    const countryDetails = document.getElementById('country-details');
+    const div = document.createElement('div');
+    div.classList.add('datils')
+    div.innerHTML = `
+        <img width =200px; src = "${country.flag}"/>
+        <h3>Name: ${country.name}</h3>
+        <h4>Native Name: ${country.nativeName}</h4>
+        <h4>Capital: ${country.capital}</h4>
+        <p>Population: ${country.population}</p>
+        <P>Region: ${country.region}</p>
+        <P>Sub Region: ${country.subregion}</p>
+
+    `;
+    countryDetails.appendChild(div)
 }
