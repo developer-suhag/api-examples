c824fa77a58aac9b75b5c76bd2081c14ef476829
developer-suhag <developersuhag@gmail.com>
2021-08-27 12:18:22 +0600

    show meals by search dynamicly

--- js/mealdb.js
+++ js/mealdb.js
@@ -5,5 +5,27 @@ const searchFood = () => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.meals))
+        .then(data => displayFood(data.meals))
+}
+
+const displayFood = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        const mealCard = document.createElement('div');
+        mealCard.classList.add('col');
+        mealCard.innerHTML = `
+        <div class="card">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">
+                ${meal.strInstructions.slice(0, 200)}...
+                </p>
+            </div>
+        </div>
+        `;
+
+
+        searchResult.appendChild(mealCard);
+    });
 }
