13f1f1594bc24d7d34af08566c9c92b554d98e3c
developer-suhag <developersuhag@gmail.com>
2021-08-27 17:23:24 +0600

    error handle

--- js/mealdb.js
+++ js/mealdb.js
@@ -2,14 +2,22 @@ const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayFood(data.meals))
+    if (searchText == '') {
+        document.getElementById('search-error').style.display = 'block'
+
+    } else {
+        document.getElementById('search-error').style.display = 'none'
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayFood(data.meals))
+    }
+
 }
 
 const displayFood = meals => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     meals.forEach(meal => {
         // console.log(meal);
         const mealCard = document.createElement('div');
@@ -42,17 +50,20 @@ const loadMealDetail = mealId => {
 const showDetails = meal => {
     console.log(meal);
     const mealDetails = document.getElementById('meal-details');
+    mealDetails.textContent = '';
     const mealCard = document.createElement('div');
     mealCard.classList.add('card');
     mealCard.innerHTML = `
         <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
-        <div class="card-body">
-            <h5 class="card-title">${meal.strMeal}</h5>
-            <p class="card-text">
-            ${meal.strInstructions.slice(0, 300)}
-            </p>
-            <a href="${meal.strYoutube}" target="_blank" class="btn btn-primary">Go somewhere</a>
-        </div>
-    `;
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">
+                ${meal.strInstructions.slice(0, 300)}
+                </p>
+                <a href="${meal.strYoutube}" target="_blank" class="btn btn-primary">Go somewhere</a>
+            </div>
+        `;
+
+
     mealDetails.appendChild(mealCard);
 }
